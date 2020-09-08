5815fb1838a547aaed0f93a99451ada0c242b072
Demon33 <bindhu2325@gmail.com>
2020-09-08 15:16:26 -0400

    Update index.js

--- completed_exercises/maxchar/index.js
+++ completed_exercises/maxchar/index.js
@@ -28,4 +28,24 @@ function maxChar(str) {
   return maxChar;
 }
 
+//or
+
+function max(str){
+const charMap = {};
+let max = 0;
+let maxChar = '';
+
+for(let i of str){
+    charMap[i] = charMap[i] + 1 || 1;
+}
+
+for(let i in charMap){
+    if(charMap[i] > max){
+    max = charMap[i];
+    maxChar = i;
+    }
+}
+return maxChar;
+}
+
 module.exports = maxChar;
